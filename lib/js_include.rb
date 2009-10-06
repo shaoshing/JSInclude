f83be43012e1dcfad8e9ba77e720016917607495
SXLee <sxlee87@gmail.com>
2009-10-06 11:43:04 +0800

    实现Helper测试

--- lib/js_include.rb
+++ lib/js_include.rb
@@ -1,1 +1,15 @@
-# JSInclude
+
+module JSInclude
+  
+  module Helper
+    def self.js_include
+      JSInclude.get_required_file_names.each do |file|
+        javascript_include_tag file
+      end
+    end
+  end
+  
+  def self.get_required_file_names
+  end
+  
+end
--- spec/js_include_spec.rb
+++ spec/js_include_spec.rb
@@ -1,8 +1,21 @@
 
 require "lib/js_include"
 
-describe "" do
-  it "should success" do
-    true.should == true
+describe "JSInclude" do
+  
+  describe "view helper [js_include] " do
+    it "should be defined" do
+      JSInclude::Helper.respond_to?(:js_include).should be_true 
+    end
+    
+    it "should call 'javascript_include_tag' for each required file " do
+      JSInclude.should_receive(:get_required_file_names).and_return ["a.js","b.js"]
+      JSInclude::Helper.should_receive(:javascript_include_tag).with("a.js")
+      JSInclude::Helper.should_receive(:javascript_include_tag).with("b.js")
+      
+      JSInclude::Helper.js_include
+    end
+    
   end
+  
 end
