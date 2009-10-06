c0511293004c54f40bf85c7b702d157e9b45fb57
SXLee <sxlee87@gmail.com>
2009-10-06 11:50:47 +0800

    开始编写文件扫描的部分

--- lib/js_include.rb
+++ lib/js_include.rb
@@ -2,14 +2,19 @@
 module JSInclude
   
   module Helper
-    def self.js_include
-      JSInclude.get_required_file_names.each do |file|
+    def self.js_include file_name
+      JSInclude.get_required_file_names(file_name).each do |file|
         javascript_include_tag file
       end
     end
   end
   
-  def self.get_required_file_names
+  def self.get_required_file_names file_name
+    scan_include_tag file_name
+  end
+  
+  def self.scan_include_tag file_name
+    
   end
   
 end
--- spec/js_include_spec.rb
+++ spec/js_include_spec.rb
@@ -1,4 +1,9 @@
 
+# Run Test : 
+#   cd    path_to_JSInclude/
+#   spec  spec/js_include_spec.rb
+
+
 require "lib/js_include"
 
 describe "JSInclude" do
@@ -8,14 +13,26 @@ describe "JSInclude" do
       JSInclude::Helper.respond_to?(:js_include).should be_true 
     end
     
-    it "should call 'javascript_include_tag' for each required file " do
-      JSInclude.should_receive(:get_required_file_names).and_return ["a.js","b.js"]
+    it "should call [javascript_include_tag] for each required files " do
+      JSInclude.should_receive(:get_required_file_names).with("empty").and_return ["a.js","b.js"]
       JSInclude::Helper.should_receive(:javascript_include_tag).with("a.js")
       JSInclude::Helper.should_receive(:javascript_include_tag).with("b.js")
       
-      JSInclude::Helper.js_include
+      JSInclude::Helper.js_include "empty"
     end
-    
   end
+  
+  describe "[get_required_file_names]" do
+    it "should call [scan_include_tag]" do
+      JSInclude.should_receive(:scan_include_tag).with "name"
+      JSInclude.get_required_file_names "name"
+    end
+  end
+  
+  describe "[scan_include_tag]" do
+    it "should scan specify file for include tag '@include' and return file names" do
+    end
+  end
+  
   
 end
